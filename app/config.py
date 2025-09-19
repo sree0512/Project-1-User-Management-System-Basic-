eb8de7f0150e2221b4806c2972d2d2ab1cf64f13
sree0512 <157263049+sree0512@users.noreply.github.com>
2025-09-19 14:29:00 -0500

    Update getenviron

--- app/config.py
+++ app/config.py
@@ -4,13 +4,13 @@ import os
 from dotenv import load_dotenv
 from datetime import timedelta
 
-load_dotenv()
+# load_dotenv()
 
 class Config:
-    DATABASE_URL=os.getenv("DATABASE_URL")
+    DATABASE_URL=os.environ.get("DATABASE_URL")
     
-    SECRET_KEY= os.getenv("SECRET_KEY")
-    JWT_SECRET_KEY= os.getenv("JWT_SECRET")
+    SECRET_KEY= os.environ.get("SECRET_KEY")
+    JWT_SECRET_KEY= os.environ.get("JWT_SECRET")
     
      # JWT config
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  # token expires in 1 hour
